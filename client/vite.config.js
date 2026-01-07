8f1f981ed220ea8a9e16184c247d966f79185b20
Z-Isaac08 <ezechielncho225@gmail.com>
2026-01-07 17:56:35 +0000

    refactor: remove nginx configuration and unused static copy plugin from Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,25 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.',
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
     port: 5173,
